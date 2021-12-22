107e1e8108144c23e151ae831a848068b5d43996
Mangin Maia <mmangin@srv-tp04.insa-toulouse.fr>
2021-12-22 22:51:56 +0100

    Trying to fix the problem Unknown line

--- src/tasksassignment.ml
+++ src/tasksassignment.ml
@@ -11,72 +11,102 @@ It needs information about students and tasks.*)
 (*Input files format :
 - create a student : "student s n" where s is the student's number and n is the number of tasks that they can do 
 - create a task : "task t n" where t is the task's number et n is the number of students needed to achieve this task
-- define what kind of task a student can do : "association s t" where s is the student's number and t is the task's number
+                                                                                                   - define what kind of task a student can do : "association s t" where s is the student's number and t is the task's number
 
 Example :
 student 1 2
 student 2 1
 student 3 3
 student 4 3
-task 1 3
-task 2 3
-task 3 2
-association 1 1
-association 1 2
-association 2 3
-association 3 1
-association 3 2
-association 3 3
-association 4 1
-association 4 2
-association 4 3 *)
+task 5 3
+task 6 3
+task 7 2
+association 1 5
+association 1 6
+association 2 7
+association 3 5
+association 3 6
+association 3 7
+association 4 5
+association 4 6
+association 4 7 *)
 
 let source = (-1)
-let sink = 0
+let sink = (-2)
 let default_label = 1
-let init_graph = new_node (new_node empty_graph source) sink
+
 
 
 (*Function that creates a node for each student and an arc from source to student with label "number of tasks that this student can do"*)
 let read_students graph line = 
-   Scanf.sscanf line "student %d %d" (fun student nb_tasks -> new_arc (new_node graph student) source student nb_tasks)
-    
-(* let test_read_students = (fun phrase -> read_students (new_node empty_graph source) phrase) *)
+  try 
+    Scanf.sscanf line "student %d %d" (fun student nb_tasks -> new_arc (new_node graph student) source student nb_tasks) 
+  with e ->
+    Printf.printf "Impossible to read student in line : \n%s\n" line ; 
+    failwith "from_file"
+
+
 
 (*Function that creates a node for each task and an arc from task to sink with label "number of students needed to achieve that task"*)
-let read_tasks graph line = 
-   Scanf.sscanf line "task %d %d" (fun task nb_students -> new_arc (new_node graph task) task sink nb_students)
+let read_tasks graph line =
+  try 
+    Scanf.sscanf line "task %d %d" (fun task nb_students -> new_arc (new_node graph task) task sink nb_students)
+  with e ->
+    Printf.printf "Impossible to read task in line : \n%s\n" line ; 
+    failwith "from_file"
     
-(* let test_read_tasks = (fun graph phrase -> read_tasks (new_node graph sink) phrase) *)
 
 
 (*Function that creates an arc for each association with default label 1*)
 let read_associations graph line = 
-   Scanf.sscanf line "association %d %d" (fun student task -> new_arc graph student task default_label)
-    
-(*let test_read_associations = (fun graph phrase -> read_associations graph phrase) *)
+  try 
+    Scanf.sscanf line "association %d %d" (fun student task -> new_arc graph student task default_label) 
+  with e ->
+    Printf.printf "Impossible to read association in line : \n%s\n" line ; 
+    failwith "from_file"
+      
+      
 
-
+(* Function that reads a comment *)
+let read_comment graph line =
+  try
+    Scanf.sscanf line "%%" graph
+  with _ ->
+    Printf.printf "Unknown line : \n%s\n" line ; 
+    failwith "from_file"
+   
+      
+  
 (* Function that creates a graph from a source file *)
 let read_file file =
-
+  (* Open the file *)
   let open_file = open_in file in 
 
+  (* Initialize the graph *)
+  let init_graph = new_node (new_node empty_graph source) sink in
+  
   (* Read all lines until end of file *)
   let rec loop graph =
     try
       (* Read a line *)
       let line = input_line open_file in 
       
+      (* Remove spaces from line*)
+      let line = String.trim line in
+      
       let graph_aux = 
-      match line with 
-        | "student %d %d" -> read_students graph line
-        | "task %d %d" -> read_tasks graph line
-        | "association %d %d" -> read_associations graph line
-        | _ -> read_students graph line
+        
+        (* Ignore empty lines *)
+        if line = "" then graph
+          
+        else match line.[0] with 
+          | 's' -> read_students graph line
+          | 't' -> read_tasks graph line
+          | 'a' -> read_associations graph line
+          | _ -> read_comment graph line
       in
-      loop graph_aux
-
+      loop graph_aux 
+    
     with End_of_file -> graph 
 
   in
@@ -85,3 +115,4 @@ let read_file file =
 
   close_in open_file ;
   result
+
